0935acdf5bd25cea5c0d8ab43b44a925f6887c9a
Samuel Brandão <37352459+SamuelBrandao08@users.noreply.github.com>
2022-12-03 19:38:44 -0300

    arquivos de teste deletados

--- greeter/migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-var Greeter = artifacts.require("Greeter");
-var MyContract = artifacts.require("MyContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Greeter);
-  deployer.deploy(MyContract);
-};
